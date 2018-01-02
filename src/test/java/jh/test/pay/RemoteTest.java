59e5ef32b90efae68ae5d3a29e6c46693944b92e
tengfei <tengfei@tengfeideMacBook-Pro.local>
2018-01-02 17:09:03 +0800

    add

--- src/test/java/jh/test/pay/RemoteTest.java
+++ src/test/java/jh/test/pay/RemoteTest.java
@@ -2,6 +2,7 @@ package jh.test.pay;
 
 import hf.base.utils.Utils;
 import jh.test.BaseCommitTestCase;
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
@@ -36,5 +37,19 @@ public class RemoteTest extends BaseCommitTestCase {
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> entity = restTemplate.postForEntity("http://127.0.0.1:8080/jh/pay/unifiedorder",payParams,String.class, new Object[0]);
         System.out.println(entity.getBody());
+    }
+
+    @Test
+    public void testSign() {
+        Map<String,Object> map = new HashMap<>();
+        map.put("out_trade_no","5139_20180102164506104");
+        map.put("service","pay.weixin.jspay");
+        map.put("total_fee","1000000");
+        map.put("body","转账10000");
+        map.put("mch_id","102555074371");
+        map.put("sub_openid","ojoKev39p15cuREQKbNnRSmjN9EY");
+
+        String sign = Utils.encrypt2(map,"d4653889e27b45fb51bae4eb427c1a92");
+        Assert.assertEquals("15452C3B22FFBFF177254024731B8850",sign);
     }
 }
