58de9d708a6e219aafefbc9327894ae8ca60af29
tengfei <tengfei@tengfeideMacBook-Pro.local>
2018-01-21 22:10:21 +0800

    add

--- src/test/java/jh/test/page/PageTest.java
+++ src/test/java/jh/test/page/PageTest.java
@@ -8,7 +8,9 @@ import jh.test.BaseTestCase;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by tengfei on 2017/10/29.
@@ -28,5 +30,27 @@ public class PageTest extends BaseTestCase {
     @Test
     public void testGetCipherCode() {
         System.out.println(Utils.getRandomString(8));
+    }
+
+    @Test
+    public void testGetRandom() {
+        Map<String,Object> param = new HashMap<>();
+        param.put("total","1200");
+        param.put("buyer_id","");
+        param.put("merchant_no","5151");
+        param.put("remark","");
+        param.put("sub_open_id",null);
+        param.put("sign_type","MD5");
+        param.put("create_ip","127.0.0.1");
+        param.put("nonce_str","1516542284541363");
+        param.put("out_trade_no","1516542284541363");
+        param.put("version","1.0");
+        param.put("name","Lotto");
+        param.put("service","02");
+        param.put("auth_code",null);
+        param.put("outlet_no",null);
+
+        String sign = Utils.encrypt(param,"y6sfdfdf");
+        System.out.println(sign);
     }
 }
